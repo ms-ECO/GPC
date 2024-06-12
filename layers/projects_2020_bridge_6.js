a9ffad2642bdbc13e235a165da7c4ed2a95032a2
ms-ECO <martin.schweter@eco-consult.com>
2024-06-12 18:14:08 +0200

    Add files via upload

---
+++ layers/projects_2020_bridge_6.js
@@ -0,0 +1,8 @@
+var json_projects_2020_bridge_6 = {
+"type": "FeatureCollection",
+"name": "projects_2020_bridge_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
---
+++ layers/projects_2020_bridge_9.js
@@ -0,0 +1,1 @@
+var json_projects_2020_bridge_9 = {"type":"FeatureCollection","name":"projects_2020_bridge_9","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
