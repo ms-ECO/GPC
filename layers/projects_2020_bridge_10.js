2fa3fe89e5392fb97dc3f3f35ab3432bdc2fc13a
ms-ECO <martin.schweter@eco-consult.com>
2024-06-13 09:39:11 +0200

    Delete layers directory

--- layers/projects_2020_bridge_10.js
+++
@@ -1,1 +0,0 @@
-var json_projects_2020_bridge_10 = {"type":"FeatureCollection","name":"projects_2020_bridge_10","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
--- layers/projects_2020_bridge_9.js
+++
@@ -1,1 +0,0 @@
-var json_projects_2020_bridge_9 = {"type":"FeatureCollection","name":"projects_2020_bridge_9","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
